9821cd026349ce0ab31434d285ecf55b1bb2f4b6
Aman9das <amandas62640@gmail.com>
2024-06-06 13:15:08 +0530

    remove unnecessary improvements

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,7 +1,3 @@
-if true then
-  return {}
-end
-
 return {
   {
     "rcarriga/nvim-notify",
@@ -31,18 +27,9 @@ return {
       window = {
         mappings = {
           ["="] = "cd_root",
-          ["o"] = "system_open",
         },
       },
       commands = {
-        system_open = function(state)
-          local node = state.tree:get_node()
-          local path = node:get_id()
-          -- macOs: open file in default application in the background.
-          -- vim.fn.jobstart({ "xdg-open", "-g", path }, { detach = true })
-          -- Linux: open file in default application
-          vim.fn.jobstart({ "xdg-open", path }, { detach = true })
-        end,
         cd_root = function(state)
           local path = state.path -- this gives you the path
 
