632ce87a9ba99081c85f3a4314e6b1696f724d1b
Aman9das <amandas62640@gmail.com>
2024-06-16 20:16:03 +0530

    remove unnecessary ftplugins

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -24,6 +24,12 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
+      sync_root_with_cwd = true,
+      respect_buf_cwd = true,
+      update_focused_file = {
+        enable = true,
+        update_root = true,
+      },
       window = {
         mappings = {
           ["="] = "cd_root",
